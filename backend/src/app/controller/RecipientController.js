6437390a53805e014c15545463866fac7b0c9a7e
tiagosn01 <tiagosilva0922@gmail.com>
2020-02-19 21:55:38 -0300

    Add midleware, control admin, update recipient

--- backend/src/app/controller/RecipientController.js
+++ backend/src/app/controller/RecipientController.js
@@ -1,13 +1,41 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
+import User from '../models/User';
 
 class RecipientController {
   async store(req, res) {
-    const userExists = await Recipient.findOne({
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    // Check User is admin
+    const isAdmin = await User.findOne({
+      where: { id: req.userId, provider: false },
+    });
+
+    if (!isAdmin) {
+      return res.status(401).json('User is not an admin');
+    }
+
+    const recipientExists = await Recipient.findOne({
       where: { email: req.body.email },
     });
 
-    if (userExists) {
-      return res.status(400).json({ error: 'O usuário ja existe.' });
+    if (recipientExists) {
+      return res.status(401).json({ error: 'Recipient already exists.' });
     }
 
     const {
@@ -29,6 +57,60 @@ class RecipientController {
       complement,
       state,
       city,
+      zipcode,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    // Check User is admin
+    const isAdmin = await User.findOne({
+      where: { id: req.userId, provider: false },
+    });
+
+    if (!isAdmin) {
+      return res.status(401).json('User is not an admin');
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient does not exists' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zipcode,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
       zipcode,
     });
   }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,13 +2,17 @@ import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import RecipientController from './app/controller/RecipientController';
 import SessionController from './app/controller/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
-routes.post('/recipients', RecipientController.store);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
